27395af7513be3eef93457d8a135e84eecc48036
mahmoudhosni200 <20230370@stud.fci-cu.edu.eg>
2025-05-11 14:06:40 +0300

    Add Class Patient

--- problem4.cpp
+++ problem4.cpp
@@ -1,7 +1,27 @@
 // problem 4 , Mahmoud hosni and Mahmoud Hesham
-
+#include <bits/stdc++.h>
+using namespace std;
 class Heap{
-  };
+};
 
 class EmergencyRoom {
+};
+
+class Patient {
+    string name;
+    int severity, arrivalTime;
+public:
+    Patient(string name, int severity, int arrivalTime) {
+        this->name = name;
+        this->severity = severity;
+        this->arrivalTime = arrivalTime;
+    }
+    bool operator>(const Patient &other) const{
+        if(severity == other.severity)
+            return arrivalTime < other.arrivalTime;
+        return severity > other.severity;
+    }
+    string getName() const{
+        return name;
+    }
 };
