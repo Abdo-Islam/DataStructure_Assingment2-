42ef91b98de60f8acd3c786808da189b939f0127
Abdo-Islam <abdoislam2050@gmail.com>
2025-05-11 20:44:18 +0300

    .

--- da3basa.cpp
+++ da3basa.cpp
@@ -23,6 +23,14 @@
 //     //         parent->right = new Contact(node.id, node.data); 
 //     //         parent->right->parent = parent; 
 //     //     }
+    //p = new Contact(node.id, node.data); 
+//         p->parent = parent;
+//         if(p->id < parent->id){
+//             parent->left = p;
+//         }
+//         else {
+//             parent->right = p; 
+//         } 
 //     //     n++; 
 //     // }
 
