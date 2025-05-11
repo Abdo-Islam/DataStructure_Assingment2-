669c6a3ac406be2f9b7873ff87016a8529ac422a
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2025-05-11 14:09:37 +0300

    start at MaxHeap

--- problem4.cpp
+++ problem4.cpp
@@ -1,7 +1,23 @@
 // problem 4 , Mahmoud hosni and Mahmoud Hesham
+#include <vector>
+using namespace std;
+template <typename T>
+class MaxHeap{
+private:
+  vector<T> heap;
+  void heapify(int index) {
+    int parent = (index / 2) + 1;
+    if (index == 0) {
+      return;
+    }
+    if (heap[index] > heap[parent]) {
+      swap(heap[index], heap[parent]);
+      heapify(parent);
+    }
+  }
 
-class Heap{
   };
 
 class EmergencyRoom {
+
 };
